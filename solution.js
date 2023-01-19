25b7455eaf1484c94db2914f12a2e32120b7a7a8
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-19 17:09:27 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
+   let reverseString = str.split("").reverse().join("");
+    return reverseString === str ? "true" : "false";
 
 }
 module.exports = palindrome
