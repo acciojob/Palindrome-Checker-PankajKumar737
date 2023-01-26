a0f5130544d016c622d2d3ad26b6e04dbadd8602
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-26 09:28:34 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,8 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-   let reverseString = str.split("").reverse().join("");
-    return reverseString === str ? "true" : "false";
+   
+    var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+    str = str.toLowerCase().replace(re, '');
+   
+   for (var i = 0; i < str.len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) { 
+          return false;
+      }
+    return true ;
+   }   
 
 }
 module.exports = palindrome
